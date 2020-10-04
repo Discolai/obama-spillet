80ce751ea65de178564d09eb12cf55a6fe6b53a7
Nikolai Norum Hansen <nikolainh@hotmail.com>
2020-10-04 17:16:15 +0200

    Initialize project using Create React App

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: "none",
+  semi: true,
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "always"
+};
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render(): React.ReactNode {
+    return (
+      <div>
+        <div className="container">
+          <header className="text-center mt-4">
+            <h1>Hello World!</h1>
+          </header>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
