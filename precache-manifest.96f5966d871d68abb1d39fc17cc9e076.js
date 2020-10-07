80a2441d4ab1a29d3294c04c485bd73d8f123081
Nikolai Norum Hansen <nikolainh@hotmail.com>
2020-10-07 21:48:35 +0200

    Updates

---
+++ precache-manifest.96f5966d871d68abb1d39fc17cc9e076.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e16a341f13311356207f0e716e88b2f",
+    "url": "obama-spillet/index.html"
+  },
+  {
+    "revision": "4db9b3dd40af8c3a8926",
+    "url": "obama-spillet/static/css/main.e47d98b1.chunk.css"
+  },
+  {
+    "revision": "5f442cceccc8a81c5eca",
+    "url": "obama-spillet/static/js/2.772376c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "obama-spillet/static/js/2.772376c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4db9b3dd40af8c3a8926",
+    "url": "obama-spillet/static/js/main.dbbed3a0.chunk.js"
+  },
+  {
+    "revision": "e7ccccb7a2a071b3df87",
+    "url": "obama-spillet/static/js/runtime-main.93d8e5e4.js"
+  },
+  {
+    "revision": "8ced8f718436352958af98580dc79a4f",
+    "url": "obama-spillet/static/media/american-flag.8ced8f71.png"
+  },
+  {
+    "revision": "17e3cc8997c9145535919e4c60257883",
+    "url": "obama-spillet/static/media/obama-sangen.17e3cc89.mp3"
+  },
+  {
+    "revision": "1bb75a73745ec2f6553989c1ab66e5fb",
+    "url": "obama-spillet/static/media/obama.1bb75a73.png"
+  }
+]);
